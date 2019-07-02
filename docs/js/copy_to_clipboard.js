4e48baacd3045eb0fc6d7780c476e068665f22a8
brunomateus <brunomateus@gmail.com>
2019-07-02 19:11:55 +0200

    Adding post about first publication

---
+++ docs/js/copy_to_clipboard.js
@@ -0,0 +1,18 @@
+
+
+var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('.js-copytextarea');
+  copyTextarea.focus();
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+    M.toast({html: 'Copied to the clipboard with success.'});
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
